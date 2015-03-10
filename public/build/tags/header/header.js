5aa04b45abef08419d4581eb4d9a406fbc90713e
kysonic <soooyc@gmail.com>
2015-03-10 18:20:25 +0300

    Webstarter first.

--- public/build/tags/header/header.js
+++
@@ -1,6 +0,0 @@
-loadCss("./build/tags/header/header.css");
-define(['riot'],function(riot){
-   return riot.tag('header', '<div id="logo">{logoText}</div>', function cb(opts) {
-    this.logoText = opts.logoText;
-});
-});
